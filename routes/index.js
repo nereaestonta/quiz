7aa91dd296264287c04e8b87320c7f3a583326df
nereaestonta <nnavarro@ESLNERNAV.offilog.local>
2015-08-10 09:29:26 +0200

    Comment moderation

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
 });
 
-// Autoload de comandos con :quizId 
+// Autoload de comandos con :quizId
 router.param('quizId', quizController.load); 		// autoload :quizId
+router.param('commentId', commentController.load); 	// autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login', 						sessionController.new);
@@ -29,7 +30,9 @@ router.post('/quizes/create', 				sessionController.loginRequired, quizControlle
 router.put('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.destroy);
 
+// Definición de rutas de /comments
 router.get('/quizes/:quizId(\\d+)/comments/new', 		commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', 			commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish); 		// must be PUT
 
 module.exports = router;
